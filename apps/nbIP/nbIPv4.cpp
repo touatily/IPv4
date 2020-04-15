965d31fbecad086ccba1eb3e243790d66fd50aef
touatily <lyes.touati@gmail.com>
2020-04-15 03:27:47 +0200

    nb IP first version

---
+++ apps/nbIP/nbIPv4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ipv4>
+#include <string>
+#include <sstring>
+
+using namespace std;
+
+int main(int argc, char ** argv) {
+
+	if(argc == 1){
+		return -1;
+	}
+	else{
+		for(int i = 1; i < argc; i++){
+			string s(argv[i]);
+			streamstring ss;
+			ss << s;
+			if(ss.)
+		}
+		unsigned long long nb = IPv4::nbIPv4()
+	}
+	return 0;
+}
